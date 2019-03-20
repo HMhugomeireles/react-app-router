0e947c57e02e76239481c46aeda0ba9153426731
Hugo Meireles <meireles.hmm@gmail.com>
2019-03-20 00:42:39 +0000

    first test of react routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+const Topics = () => (
+  <div>
+    <h2>topics</h2>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>><Link to="/"> Home </Link></li>
+            <li>><Link to="/about"> About</Link></li>
+            <li>><Link to="/topics"> Topics</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/topics" component={Topics} />
+
+        </div>
+      </Router>
     );
   }
 }
